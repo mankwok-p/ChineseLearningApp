0c9797695dac5e2cbab984a88f66d54fd3b667c3
Kwok <astleykwok@gmail.com>
2019-03-08 23:18:00 +0800

    Play sound when click button

---
+++ App/Assets/Scripts/DontDestory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static DontDestory instance = null;
+    public static DontDestory Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    // any other methods you need
+
+}
--- App/Assets/Scripts/MainBtnHandler.cs
+++ App/Assets/Scripts/MainBtnHandler.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+[RequireComponent(typeof(Button))]
 public class MainBtnHandler : MonoBehaviour
 {
-    public void StartGame() {
-        SceneManager.LoadScene("GameSelect");
+    public AudioClip sound;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
     }
 
-    public void exitGame(){
-		Application.Quit();
-	}
+    void PlaySound()
+    {
+        source.PlayOneShot(sound);
+    }
+
+    public void StartGame()
+    {
+        StartCoroutine(DelaySceneLoad("GameSelect"));
+    }
+
+    public void BackToHome()
+    {
+        StartCoroutine(DelaySceneLoad("MainMenu"));
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator DelaySceneLoad(string sceneName)
+    {
+        PlaySound();
+        yield return new WaitForSeconds(sound.length);
+        SceneManager.LoadScene(sceneName);
+
+    }
 }
