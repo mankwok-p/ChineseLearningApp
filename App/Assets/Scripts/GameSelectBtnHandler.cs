c10e33d31f7346238936db106ed0cab018d35b49
Kwok <astleykwok@gmail.com>
2019-03-09 12:53:41 +0800

    Buttons for selecting games

---
+++ App/Assets/Scripts/GameSelectBtnHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+[RequireComponent(typeof(Button))]
+public class GameSelectBtnHandler : MonoBehaviour
+{
+    public AudioClip sound;
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
+    }
+
+    void PlaySound()
+    {
+        source.PlayOneShot(sound);
+    }
+
+    IEnumerator DelaySceneLoad(string sceneName)
+    {
+        PlaySound();
+        yield return new WaitForSeconds(sound.length);
+        SceneManager.LoadScene(sceneName);
+
+    }
+}
