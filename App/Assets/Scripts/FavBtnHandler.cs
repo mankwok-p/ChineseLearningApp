4ea7b43cfb037198728a157b01ad703040af922a
Kwok <astleykwok@gmail.com>
2019-03-30 10:32:09 +0800

    AB#72 Fav button script

---
+++ App/Assets/Scripts/FavBtnHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FavBtnHandler : MonoBehaviour
+{
+    private UserData userData;
+
+    void Start()
+    {
+        this.userData = UserData.LoadUserData();
+    }
+
+    public void OnFavBtnCliked(string gameName) 
+    {
+        this.userData.HandleFavouriteGame(gameName);
+    }
+}
